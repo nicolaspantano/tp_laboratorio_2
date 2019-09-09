3f3760dea74fc923ac1dff1ba11670710de94e6a
nicolaspantano <47321352+nicolaspantano@users.noreply.github.com>
2019-09-09 20:28:17 -0300

    Add files via upload

---
+++ Tp01.Pantano.Nicolas/Entidades/Calculadora.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            operador = ValidarOperador(operador);
+
+            switch (operador)
+            {
+                case "+":
+                    return num1 + num2;
+                    
+
+                case "-":
+                    return num1 - num2;
+                    
+
+                case "/":
+                    return num1 / num2;
+
+
+                    //*
+                default:
+                    return num1 * num2;
+                    
+
+
+            }
+        }
+
+        private static string ValidarOperador(string operador)
+        {
+            if (operador == "+" || operador == "-" || operador == "/" || operador == "*")
+            {
+                return operador;
+            }
+            else
+            {
+                return "+";
+            }
+        }
+    }
+}
---
+++ Tp01.Pantano.Nicolas/Entidades/Numero.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        private double ValidarNumero(string strNumero)
+        {
+            double salida;
+            if (double.TryParse(strNumero, out salida))
+            {
+                return salida;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static double operator +(Numero num1,Numero num2)
+        {
+            return num1.numero + num2.numero;
+        }
+        public static double operator -(Numero num1, Numero num2)
+        {
+            return num1.numero - num2.numero;
+
+        }
+        public static double operator /(Numero num1, Numero num2)
+        {
+            if (num2.numero != 0)
+            {
+                return num1.numero + num2.numero;
+
+            }
+            else
+            {
+                return double.MinValue;
+            }
+        }
+        public static double operator *(Numero num1, Numero num2)
+        {
+            return num1.numero * num2.numero;
+
+        }
+
+
+    }
+}
