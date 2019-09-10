107556e70c5626f7f7d2419d1626eb15752479d1
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-09-09 22:04:06 -0300

    Empeze con el windows form

--- Tp01.Pantano.Nicolas/Entidades/Numero.cs
+++ Tp01.Pantano.Nicolas/Entidades/Numero.cs
@@ -11,6 +11,16 @@ namespace Entidades
             this.numero = 0;
         }
 
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+            this.numero = double.Parse(strNumero);
+        }
+
         private double ValidarNumero(string strNumero)
         {
             double salida;
@@ -22,6 +32,11 @@ namespace Entidades
             {
                 return 0;
             }
+        }
+
+        public double SetNumero
+        {
+            set { this.numero = value; }
         }
 
         public static double operator +(Numero num1,Numero num2)
@@ -48,6 +63,82 @@ namespace Entidades
         public static double operator *(Numero num1, Numero num2)
         {
             return num1.numero * num2.numero;
+
+        }
+
+        public static string DecimalBinario(double numero)
+        {
+            string binario = "";
+
+            while (numero > 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    binario = "0" + binario;
+                }
+                else
+                {
+                    binario = "1" + binario;
+                }
+
+                numero = numero / 2;
+            }
+
+
+            return binario;
+        }
+
+        public static string DecimalBinario(string numero)
+        {
+            string binario = "";
+            double convertido;
+
+
+            if (double.TryParse(numero, out convertido))
+            {
+                return DecimalBinario(convertido);
+            }
+            else
+            {
+                return "Valor invalido";
+            }
+
+           
+        }
+
+        public static string BinarioDecimal(string binario)
+        {
+            double doble = 0;
+
+            int exponente = 0;
+
+
+
+            for (int i = (binario.Length) - 1; i >= 0; i--)
+            {
+
+                if (binario[i] == '1')
+                {
+                    if (i == (binario.Length) - 1)
+                    {
+                        doble += 1;
+                        continue;
+
+                    }
+                    else
+                    {
+                        doble += Math.Pow(2, exponente);
+
+                    }
+
+                }
+
+                exponente++;
+
+            }
+
+
+            return doble.ToString();
 
         }
 
---
+++ Tp01.Pantano.Nicolas/MiCalculadora/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace MiCalculadora
+{
+    partial class FormCalculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnConvertirABinario = new System.Windows.Forms.Button();
+            this.btnConvertirADecimal = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(12, 154);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(120, 37);
+            this.btnOperar.TabIndex = 0;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(140, 154);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(120, 37);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnConvertirABinario
+            // 
+            this.btnConvertirABinario.Location = new System.Drawing.Point(202, 204);
+            this.btnConvertirABinario.Name = "btnConvertirABinario";
+            this.btnConvertirABinario.Size = new System.Drawing.Size(184, 38);
+            this.btnConvertirABinario.TabIndex = 2;
+            this.btnConvertirABinario.Text = "Convertir a Binario";
+            this.btnConvertirABinario.UseVisualStyleBackColor = true;
+            // 
+            // btnConvertirADecimal
+            // 
+            this.btnConvertirADecimal.Location = new System.Drawing.Point(12, 204);
+            this.btnConvertirADecimal.Name = "btnConvertirADecimal";
+            this.btnConvertirADecimal.Size = new System.Drawing.Size(184, 38);
+            this.btnConvertirADecimal.TabIndex = 3;
+            this.btnConvertirADecimal.Text = "Convertir a Decimal";
+            this.btnConvertirADecimal.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(266, 154);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(120, 37);
+            this.btnCerrar.TabIndex = 4;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // FormCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 249);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnConvertirADecimal);
+            this.Controls.Add(this.btnConvertirABinario);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOperar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Nicolas Pantano del curso 2°A";
+            this.Load += new System.EventHandler(this.FormCalculadora_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnConvertirABinario;
+        private System.Windows.Forms.Button btnConvertirADecimal;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
