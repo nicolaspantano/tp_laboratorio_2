98663158edebee91b88d72539a57cc56ce407b41
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-10-02 15:28:26 -0300

    tp2

---
+++ TP-02/Entidades/Leche.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2018
+{
+    public class Leche : Producto
+    {
+        public enum ETipo { Entera, Descremada }
+        ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string patente, ConsoleColor color):this(marca,patente,color,ETipo.Entera)         
+        {
+            
+        }
+        public Leche(EMarca marca, string patente, ConsoleColor color, ETipo tipo ):base(marca,patente,color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+            set
+            {
+                this.CantidadCalorias = value;
+            }
+            
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : "+this.CantidadCalorias+"TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+       
+
+
+
+    }
+}
---
+++ TP-02/Entidades/Snacks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Snacks:Producto
+    {
+        public Snacks(EMarca marca, string patente, ConsoleColor color):base(marca,patente,color)            
+        {
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+            set
+            {
+                this.CantidadCalorias = value;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);                      
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
