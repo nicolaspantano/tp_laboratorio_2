dce74bc2b6dfd1f3218440c216f18102b9489cea
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-11-25 21:14:32 -0300

    TP4

---
+++ Pantano.Nicolas.TP4/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f823584b-041c-4d17-b690-84a0d864eb10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pantano.Nicolas.TP4/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ListaPaquetesInstanciada()
+        {
+            Correo c = new Correo();
+            Assert.IsNotNull(c.Paquetes);
+        }
+
+        [TestMethod]
+        public void IgualTrackingID()
+        {
+            try
+            {
+                Correo c = new Correo();
+                Paquete p1 = new Paquete("Entrega1", "0123456789");
+                Paquete p2 = new Paquete("Entrega2", "0123456789");
+                c += p1;
+                c += p2;
+            
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(TrackingIdRepetidoException));
+
+            }
+        }
+
+
+    }
+}
